0d75836ad0f2b1a84dc1c5a47cb9ec0cb46312ba
LBechara <becharalucas@hotmail.com>
2020-01-06 02:32:38 -0300

    chama síncrona

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+var json
+
+function chama(){
+    // var promise = new Promise(function(resolve , reject))
+    var x = new XMLHttpRequest();
+    var url = "https://jsonplaceholder.typicode.com/posts"
+    var urlPacientes = "https://api-pacientes.herokuapp.com/pacientes"
+
+    x.open("GET" , url , false);
+    x.send();
+
+    var resposta = x.response;
+
+    return resposta;
+}
+
+
 function ok() {
     console.log("olá")
 }
