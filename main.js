f7f4d2bc3e3a89f91afbb8f6ec433bb2f1219c60
lbechara <becharalucas@hotmail.com>
2020-01-08 04:54:15 -0300

    tentativa de promise e adição de tabela para colocar dados do json

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 var json
 
 function chama(){
-    // var promise = new Promise(function(resolve , reject))
+    var promise = new Promise((resolve , reject) => {
     var x = new XMLHttpRequest();
-    var url = "https://jsonplaceholder.typicode.com/posts"
     var urlPacientes = "https://api-pacientes.herokuapp.com/pacientes"
-
-    x.open("GET" , url , false);
-    x.send();
-
+    
+    x.open("GET" , urlPacientes);
+    x.responseType = 'json';
+    
     var resposta = x.response;
+    
+    x.onload = () => {
+        if (x.status === 200) {
+            resolve(x.response);
+            console.log("bananas");
+        } else {
+            reject(Error('deu ruim bb:' + x.statusText));
+            console.log("não deu");
+        }
+        x.send();
+    }
 
-    return resposta;
+    });
+
+    promise.then((message) => {
+        console.log('ta aqui' + message)
+    })
 }
 
 
 function ok() {
     console.log("olá")
 }
+
+var dado = document.querySelector("#primeiro").querySelector("#n").textContent;
+// var dadoUm = dado.querySelector("#n");
+// var conteudoDado = dadoUm.textContent;
