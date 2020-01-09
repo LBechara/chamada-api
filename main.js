c494f97d16e8a036b21ab0d26b5b9a4ce230fca8
lbechara <becharalucas@hotmail.com>
2020-01-09 04:15:32 -0300

    formulário e função para add elementos do formulário na tabela

--- main.js
+++ main.js
@@ -20,12 +20,47 @@ function chama(){
     
     promise.then((resultado) => {
         console.log(resultado);
-    })
-}
+        return resultado;
+    });
+};
 
+var submit = document.querySelector("#submitform");
+submit.addEventListener("click", function addNaTabela(event) {
 
+    event.preventDefault();
+    
+    var form = document.querySelector("#form");
 
+    var nome = form.nome.value;
+    var altura = form.altura.value;
+    var peso = form.peso.value;
+    var gordura = form.gordura.value;
+    // var imc = form.imc.value;
 
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    // var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    alturaTd.textContent = altura;
+    pesoTd.textContent = peso;
+    gorduraTd.textContent = gordura;
+    // imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(gorduraTd);
+    // pacienteTr.appendChild(imcTd);
+
+    var table = document.querySelector("#table");
+
+    table.appendChild(pacienteTr);
+});
 
 function ok() {
     console.log("olá")
